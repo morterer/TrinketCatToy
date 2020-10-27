9f8dceb905261ebd12d2cc53cff09b8b1f93d9f9
Eric Morter <eric@gibson.net>
2020-10-27 13:50:50 -0500

    Added README and cleaned up comments in code

---
+++ README.md
@@ -0,0 +1,11 @@
+# Trinket Cat Toy
+A cat toy that moves a laser pointer in random directions, at random speeds.  The code chooses a random speed and position for each servo.  Each servo is moved to the target position, at the desired speed.  When the servos reach their target position, new positions and speeds are randomly chosen, and the process repeats. 
+
+## Bill of Materials
+- [Adafruit Trinket x 1](https://www.adafruit.com/product/1500) (5v version also works)
+- [Micro Servo x 2](https://www.adafruit.com/product/169)
+- [Laser Diode x 1](https://www.adafruit.com/product/1054)
+
+## Useful Links
+- [Trinket (& Gemma) Servo Control](https://learn.adafruit.com/trinket-gemma-servo-control)
+- [Adafruit SoftServo Library](https://github.com/adafruit/Adafruit_SoftServo)
--- TrinketCatToy.ino
+++ TrinketCatToy.ino
@@ -1,7 +1,7 @@
 #include <Adafruit_SoftServo.h>
 
-#define MIN_DELAY 10
-#define MAX_DELAY 50
+#define MIN_DELAY 10    // minimum servo speed
+#define MAX_DELAY 50    // maximum servo speed
 
 
 class Sweeper {
@@ -36,40 +36,37 @@ class Sweeper {
     }
 
     void update(unsigned long currentMillis) {
-      if ((currentMillis - lastUpdate) > updateInterval) { // time to update
+      if ((currentMillis - lastUpdate) > updateInterval) {  // time to update
         lastUpdate = millis();
-        if(posCurrent > posTarget){
+        if(posCurrent > posTarget){                         // compute the new position
           posCurrent --;
         } else {
           posCurrent ++;
         }
-        servo.write(posCurrent);
-        if(posCurrent == posTarget){ // if the servo is at the target position
-          updateInterval = random(MIN_DELAY,MAX_DELAY);
-          posTarget = random(posMin,posMax);
+        servo.write(posCurrent);                            // move servo to new position
+        if(posCurrent == posTarget){                        // if the servo is at the target position
+          updateInterval = random(MIN_DELAY,MAX_DELAY);     // randomly choose new speed
+          posTarget = random(posMin,posMax);                // randomly choose new position
         }
       }      
     }
 };
 
+Sweeper sweeper1(0,180);          // how far servo can move left-right
+Sweeper sweeper2(0,40);           // how far servo can move up-down
 
-Sweeper sweeper1(0,180);  // how far to sweep left-right
-Sweeper sweeper2(0,40);   // how far to sweep up-down
-
-// used for counting in the interrupt
-volatile uint8_t counter = 0;
-unsigned long currentMillis = 0;
+volatile uint8_t counter = 0;     // used for counting in the interrupt
+unsigned long currentMillis = 0;  // used to manage servo timing
 
 void setup() {
+  // Taken from Adafruit_SoftServo sample code available on Github
   // Set up the interrupt that will refresh the servo for us automagically
-  OCR0A = 0xAF;            // any number is OK
-// interrupt for Trinket
-  TIMSK |= _BV(OCIE0A);    // Turn on the compare interrupt (below!)
-// interrupt for Arduino Uno
-//  TIMSK0 |= _BV(OCIE0A);    // Turn on the compare interrupt (below!)
+  OCR0A = 0xAF;             // any number is OK
+  // interrupt for Trinket
+  TIMSK |= _BV(OCIE0A);     // Turn on the compare interrupt (below!)
   
-  sweeper1.attach(1);
-  sweeper2.attach(4);
+  sweeper1.attach(1);       // attach servo to pin 1
+  sweeper2.attach(4);       // attach servo to pin 4
 }
 
 void loop() {
@@ -78,6 +75,7 @@ void loop() {
   sweeper2.update(currentMillis);
 }
 
+// Taken from Adafruit_SoftServo sample code available on Github
 // We'll take advantage of the built in millis() timer that goes off
 // to keep track of time, and refresh the servo every 20 milliseconds
 SIGNAL(TIMER0_COMPA_vect) {
